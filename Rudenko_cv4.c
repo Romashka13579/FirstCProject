28ccb83a3423eb3f5487f21e3109caf899be5ea6
Romashka <rroma13579@gmail.com>
2023-10-23 22:14:05 +0200

    cv4, u1,u2,u3

--- Rudenko_cv4.c
+++ Rudenko_cv4.c
@@ -46,10 +46,65 @@ int cv4_druhe(){
     fclose(ffile);
 }
 
+int cv4_tretie (){
+    FILE *ffile;
+    int n;
+
+    ffile = fopen("tretia.txt", "w");
+    printf("Zadajte pocet riadkov Floydovho trojuholnika od 1 do 15: ");
+    scanf("%d", &n);
+    if(n>=1 && n<=15){
+        fprintf(ffile, "Ukazkovy vstup:\n");
+        for(int i = 0; i<n; i++){
+            for(int j = 1; j<=n-i; j++){
+                fprintf(ffile, "%d ", j);
+            }
+            fprintf(ffile, "\n");
+        }
+    }
+    else{printf("Nespravny rozmery");}
+    fclose(ffile);
+}
+
+int cv4_stvrte(){
+    FILE *ffile;
+    int n;
+
+    ffile = fopen("stvrta.txt", "w");
+    printf("Zadajte pocet riadkov Floydovho trojuholnika od 1 do 15: ");
+    scanf("%d", &n);
+    if(n>=1 && n<=15){
+        fprintf(ffile, "Ukazkovy vstup:\n");
+        for(int i = 0; i<n; i++){
+            for(int j = n-i; j>0; j--){
+                fprintf(ffile, "%d ", j);
+            }
+            fprintf(ffile, "\n");
+        }
+    }
+    else{printf("Nespravny rozmery");}
+    fclose(ffile);
+}
+
+int cv4_piate(){
+    int n, sum = 0, sum0;
+
+    scanf("%d", &n);
+    for(int i = 1; i<=n; i++){
+        sum = sum+i;
+        if(sum%3 == 0){sum0 = sum;}
+    }
+    printf("%d", sum0)
+}
+
 int main(){
-    cv4_prve();
+    /*cv4_prve();
     clearbuffer();
     cv4_druhe();
     clearbuffer();
+    cv4_tretie();
+    clearbuffer();
+    cv4_stvrte();
+    clearbuffer();*/
     return 0;
 }
