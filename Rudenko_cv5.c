f031ef37118b783c229a5bad7a9df2f1cbe1513f
Roma <93726401+Romashka13579@users.noreply.github.com>
2023-10-24 10:24:44 +0100

    Add files via upload

---
+++ Rudenko_cv5.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+void clearbuffer() {
+    while ((getchar()) != '\n');
+}
+
+int cv5_prve(){
+    FILE *ffile;
+    char c;
+    int c1 = 0;
+    ffile = fopen("vystup.txt", "w");
+    while((c = getchar()) != '\n'){
+        if(c == '.') c1 = 1;
+        if(c1 == 0) fprintf(ffile, "%c", ((c>='A' && c<='Z') ? (c - 'A' + 'a') : (c>='a' && c<='z') ? '.' : (c>='0' && c<='9') ? '-' : (c == '\n') ? '\n' : '*'));
+    }
+    fclose(ffile);
+}
+
+int cv5_druhe(){
+    FILE *ffile;
+    char c;
+    ffile = fopen("cisla.txt", "r");
+    fscanf("%d %d")
+}
+
+int main()
+{
+    cv5_prve();
+    clearbuffer();
+    cv5_druhe();
+    clearbuffer();
+    return 0;
+}
